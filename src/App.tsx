17cf2f58139432e4e0afd56f2f7ead1b5f6d2441
Divyansh Sharma <divyanshsharma4507@gmail.com>
2024-07-22 21:11:45 +0530

    Makin music player

---
+++ src/App.tsx
@@ -0,0 +1,52 @@
+import React, {useState, useEffect} from 'react';
+
+import {
+  ActivityIndicator,
+  SafeAreaView,
+  StatusBar,
+  StyleSheet,
+  Text,
+} from 'react-native';
+
+import {setupPlayer, addTrack} from '../musicPlayerService';
+
+function App(): React.JSX.Element {
+  const [isPlayerReady, setIsPlayerReady] = useState(false);
+
+  async function setup() {
+    let isSetup = await setupPlayer();
+
+    if (isSetup) {
+      await addTrack();
+    }
+
+    setIsPlayerReady(isSetup);
+  }
+
+  useEffect(() => {
+    setup();
+  }, []);
+
+  if (!isPlayerReady) {
+    return (
+      <SafeAreaView>
+        <ActivityIndicator />
+      </SafeAreaView>
+    );
+  }
+
+  return (
+    <SafeAreaView>
+      <StatusBar />
+      <Text>Hello react nativee</Text>
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+});
+
+export default App;
