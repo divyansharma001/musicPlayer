f104a4860aca96efaa496170f76e03841b367d80
Divyansh Sharma <divyanshsharma4507@gmail.com>
2024-07-29 01:15:11 +0530

    Made control center

---
+++ src/components/ControlCenter.tsx
@@ -0,0 +1,60 @@
+import { Pressable, StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+import TrackPlayer, { State, usePlaybackState } from 'react-native-track-player'
+import Icons from 'react-native-vector-icons/MaterialIcons'
+import { playbackService } from '../../musicPlayerService'
+
+
+const ControlCenter = () => {
+
+    const playBackState = usePlaybackState()
+    
+    //next button
+    const skipToNext = async () => {
+        await TrackPlayer.skipToNext()
+    }
+
+    //previous button
+    const skipToPrevious = async () => {
+        await TrackPlayer.skipToPrevious()
+    }
+
+    const togglePlayback = async(playback: State) =>{
+     const currentTrack = await TrackPlayer.getActiveTrackIndex()
+
+     if(currentTrack!=null){
+        if(playback === State.Paused || playback === State.Ready){
+            await TrackPlayer.play()
+        } else{
+            await TrackPlayer.pause()
+        }
+     }
+    }
+
+
+  return (
+    <View style={styles.container}>
+    <Pressable onPress={skipToPrevious}>
+        <Icons style={styles.icon} name="skip-previous" size={40} />
+    </Pressable>
+    </View>
+  )
+}
+
+export default ControlCenter
+
+const styles = StyleSheet.create({
+    container: {
+      marginBottom: 56,
+  
+      flex: 1,
+      flexDirection: 'row',
+      alignItems: 'center',
+    },
+    icon: {
+      color: '#FFFFFF',
+    },
+    playButton: {
+      marginHorizontal: 24,
+    },
+  });
